9d6c425dbafc78e984d7d2bf495e7a9061057afb
Orlando de Frias <realmadrid2727@yahoo.es>
2021-02-13 23:13:32 -0500

    Add db connection to app start

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const sequelize = require('./sequelize')
 const hbs = require('hbs')
 const app = express()
 const port = 3000
@@ -8,15 +9,31 @@ const port = 3000
 app.set('view engine', 'hbs');
 hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 
+async function assertDatabaseConnectionOk() {
+	console.log(`Checking database connection...`);
+	try {
+		await sequelize.authenticate();
+		console.log('Database connection OK!');
+	} catch (error) {
+		console.log('Unable to connect to the database:');
+		console.log(error.message);
+		process.exit(1);
+	}
+}
 
+async function init() {
+	await assertDatabaseConnectionOk();
 
-// get and post routes
-const homeRoutes = require("./routes/homeRoutes")
-app.use('/', homeRoutes)
+  // get and post routes
+  const homeRoutes = require("./routes/homeRoutes")
+  app.use('/', homeRoutes)
 
-const userRoutes = require("./routes/userRoutes")
-app.use('/', userRoutes)
+  const userRoutes = require("./routes/userRoutes")
+  app.use('/', userRoutes)
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+  })
+}
+
+init();

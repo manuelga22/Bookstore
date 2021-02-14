e6be7b487ecab8be4888b30c9209cceae3191e80
Orlando de Frias <realmadrid2727@yahoo.es>
2021-02-13 23:07:22 -0500

    Fixed db setup script so it correctly seeds book with author id

--- sequelize/setup.js
+++ sequelize/setup.js
@@ -7,28 +7,28 @@ async function reset() {
 
 	await sequelize.models.Author.bulkCreate([
 		{ name: 'Donald Knuth', bio: "The OG programmer." }
-	]);
-
-	await sequelize.models.Book.bulkCreate([
-		{ 
-      title: 'The Art of Computer Programming, Vol 1',
-      authorId: 1,
+  ]);
+  
+  for (const author of await sequelize.models.Author.findAll()) {
+    await sequelize.models.Book.create({
+			title: 'The Art of Computer Programming, Vol 1',
+      AuthorId: author.id,
       genre: 'Programming',
       releaseDate: new Date('August 19, 1968 23:15:30').getDate(),
       priceCents: 3000,
       coverUrl: 'https://images-na.ssl-images-amazon.com/images/I/41eCbcQARTL._SX342_BO1,204,203,200_.jpg',
       publisher: 'Addison-Wesley'
-    },
-    { 
-      title: 'The Art of Computer Programming, Vol 2',
-      authorId: 1,
+    });
+    await sequelize.models.Book.create({
+			title: 'The Art of Computer Programming, Vol 2',
+      AuthorId: author.id,
       genre: 'Programming',
       releaseDate: new Date('August 11, 1969 20:05:00').getDate(),
       priceCents: 3000,
       coverUrl: 'https://images-na.ssl-images-amazon.com/images/I/51LhI5wlKPL._SX341_BO1,204,203,200_.jpg',
       publisher: 'Addison-Wesley'
-    }
-	]);
+		});
+	}
 
 	console.log('Done!');
 }

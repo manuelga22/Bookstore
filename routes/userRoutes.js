97dd48dcce12a598b1ebe9c53dce000b01ed33cf
William Fernandez <wfern024@fiu.edu>
2021-04-09 16:20:23 -0400

    Attempting merge

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,6 +3,11 @@ const Page = require('./page');
 const { models } = require('../sequelize');
 const express = require('express');
 const router  = express.Router();
+var crypto = require('crypto');
+const sequelize = require('../sequelize');
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+
 
 
 class userRoutes extends Page{
@@ -13,43 +18,64 @@ class userRoutes extends Page{
         router.get('/sign-up', (req, res, next) => {
             res.render('users/signUp');
         });
-        router.get('/profile/:id',(req,res)=>{
-            res.render('users/profile');
-        })
+        router.get('/profile',(req,res)=>{
+            res.render('users/profile', {
+                firstName: req.session.user.firstName,
+                lastName: req.session.user.lastName,
+                email: req.session.user.email,
+                address: req.session.user.address,
+                password: req.session.user.password,
+                username: req.session.user.username
+            });
+        });
+
+
+        router.post('/register', async function(req, res, next) {
+          var formData = req.body;
+          var formEmail = formData.email;
+          var formPassword = formData.psw;
+          var salt = crypto.randomBytes(64).toString('hex');
+          var password = crypto.pbkdf2Sync(formPassword, salt, 10000, 64, 'sha512').toString('base64');
         
-      super(router);
-    }
-    router() { return router; }
+          if (!isValidPassword(password)) {
+            return res.json({status: 'error', message: 'Password must be 8 or more characters.'});
+          }
+          if (!isValidEmail(formEmail)) {
+            return res.json({status: 'error', message: 'Email address not formed correctly.'});
+          }
+        
+          try {
+            var user = await sequelize.models.User.bulkCreate([
+              {
+                firstName: " ",
+                lastName: " ",
+                username: " ",
+                password: formPassword,
+                email: formEmail,
+                address: " "
+            }
+          ]);
+          } catch (err) {
+            return res.json({status: 'error', message: 'Email address already exists.'});
+          }
+          if (user) {
+            passport.authenticate('local', function(err, user, info) {
+              if (err) { return next(err); }
+              console.log(user);
+              if (!user) {
+                return res.redirect("/");
+            
+              }
+              req.logIn(user, function(err) {
+                if (err) { return next(err); }
+                return res.redirect("/profile");
+              });
+            })(req, res, next);
+          }
+        });
 
-    
-    async logIn(){
-
-    }
-    async signUp(req,res){
-
-        var salt = crypto.randomBytes(64).toString('hex');
-        var password = crypto.pbkdf2Sync(req.body.password, salt, 10000, 64, 'sha512').toString('base64');
-      
-        if (!isValidPassword(req.body.password)) {
-          return res.json({status: 'error', message: 'Password must be 8 or more characters.'});
-        }
-        if (!isValidEmail(req.body.email)) {
-          return res.json({status: 'error', message: 'Email address not formed correctly.'});
-        }
-      
-        try {
-          var user = await User.create({
-            first_name: req.body.first_name,
-            last_name: req.body.last_name,
-            email: req.body.email,
-            role: "user",
-            password: password,
-            salt: salt
-          });
-        } catch (err) {
-          return res.json({status: 'error', message: 'Email address already exists.'});
-        }
-        if (user) {
+        
+        router.post('/login', function(req, res, next) {
           passport.authenticate('local', function(err, user, info) {
             if (err) { return next(err); }
             if (!user) {
@@ -57,25 +83,67 @@ class userRoutes extends Page{
             }
             req.logIn(user, function(err) {
               if (err) { return next(err); }
-              return res.json({status: 'ok'});
+              req.session.user = user;
+              return res.redirect("/");
             });
           })(req, res, next);
-        }
+        });
 
-    };
+        // router.post('/logout', function(req, res){
+        //   res.locals.user = false;
+        //   req.session.user = false;
+        //   res.redirect('/');
+        // });
 
-    isValidEmail(email) {
-        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-        return re.test(String(email).toLowerCase());
-    }
-    isValidPassword(password) {
+        router.post('/update', (req, res, next) => {
+
+          let updateValues = { 
+            firstName: req.body.first_name,
+            lastName: req.body.last_name,
+            email: req.body.email,
+            address: req.body.address,
+            password: req.body.password,
+            username: req.body.username
+           };
+
+           
+           if (req.body.password == req.body.password2) {
+            sequelize.models.User.update(updateValues, { where: { id: req.session.user.id } }).then((result) => {
+              // here your result is simply an array with number of affected rows
+              console.log(result);
+              // [ 1 ]
+             });
+              req.session.user.firstName = req.body.first_name,
+              req.session.user.lastName = req.body.last_name,
+              req.session.user.email = req.body.email,
+              req.session.user.address = req.body.address,
+              req.session.user.password = req.body.password
+              req.session.user.username = req.body.username
+              return res.redirect("/profile");
+            } else {
+              return res.json({status: 'error', message: 'Passwords do not match.'});
+            }
+
+
+        });
+        
+      super(router);
+
+      //checks if password has > 8 chars
+      function isValidPassword(password) {
         if (password.length >= 8) {
           return true;
         }
         return false;
-    }
-      
+      }
 
+      //uses a regex to check if email is valid
+      function isValidEmail(email) {
+        var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(String(email).toLowerCase());
+      }
+    }
+    router() { return router; }
 
 }
 
--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,10 @@ const { getIdParam, flash } = require('../helpers');
 const { models } = require('../sequelize');
 const express = require('express');
 const router = express.Router();
+var crypto = require('crypto');
+var passport = require('passport')
+  , LocalStrategy = require('passport-local').Strategy;
+var User = require('../sequelize/models/user.js').User;
 
 class Users extends Page {
   constructor() {
@@ -14,17 +18,58 @@ class Users extends Page {
     this.currentUser = models.User.build({id: 1});
 
     router.get("/api/users/:id/wish_lists", this.wishLists.bind(this));
+
+
+    passport.use(new LocalStrategy ({
+      usernameField: 'email',
+      passwordField: 'password'
+    },
+    async function(email, password, done) {
+      var user = await models.User.findOne(
+        { where: {
+            email: email,
+            password: password
+          }
+        });
+      if (user == null) {
+        return done(null, false, { message: 'Incorrect email/password.'});
+      }
+      if (!isValidPassword(password)) {
+        return done(null, false, { message: 'Incorrect email/password.' });
+      }
+      return done(null, user);
+    }
+
+    
+  ));
+
+  function isValidPassword(password) {
+    if (password.length >= 8) {
+      return true;
+    }
+    return false;
   }
+  
+  
+  }
+
+  
   
   router() { return router; }
 
   // Add more api methods here...
+
+  
+
+  
 
   async wishLists(req, res) {
     const object = await this.currentUser.wishLists();
     
     res.status(200).json(object);
   }
+
+  
 }
 
 module.exports = Users
